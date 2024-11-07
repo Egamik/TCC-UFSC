d38a418b8dc450161b5fdd21ebd72bd83a62f90d
Egami <vitor.egami@gmail.com>
2024-11-07 17:27:23 -0300

    Add inital files

---
+++ chaincode/ehr/main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"github.com/hyperledger/fabric-chaincode-go/shim"
+	peer "github.com/hyperledger/fabric-protos-go/peer"
+)
+
+type EHealth struct{}
+
+func (t *EHealth) Init(stub shim.ChaincodeStubInterface) peer.Response {
+	return shim.Success(nil)
+}
+
+func (t *EHealth) Invoke(stub shim.ChaincodeStubInterface) peer.Response {
+	// Function name and arguments
+	fn, args := stub.GetFunctionAndParameters()
+
+	switch fn {
+	case "AddIdentity":
+		if len(args) != 1 {
+			return peer.Response{
+				Status:  500,
+				Message: "Missing arguments",
+				Payload: nil,
+			}
+		}
+		return peer.Response{
+			Status:  200,
+			Payload: nil,
+		}
+	default:
+		return peer.Response{
+			Status:  500,
+			Message: "Invalid function name",
+			Payload: nil,
+		}
+	}
+}
