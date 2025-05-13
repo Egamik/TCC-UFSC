761f23902578d321ac98529b4129c0b0b704d8b1
Egami <vitor.egami@gmail.com>
2025-05-13 20:16:52 -0300

    Add files for api

--- main.sh
+++ main.sh
@@ -1,4 +1,19 @@
 #!/bin/bash
+PROJECT_ROOT = $(pwd)
+
+# Prepare chaincodes packages
+echo "Preparing chaincodes packages!"
+
+cd $PROJECT_ROOT/chaincode/accesslist
+go mod tidy
+go mod vendor
+
+cd $PROJECT_ROOT/chaincode/ehr
+go mod tidy
+go mod vendor
+
+cp -r $PROJECT_ROOT/chaincode/assets/al_asset $PROJECT_ROOT/chaincode/accesslist/vendor
+cp -r $PROJECT_ROOT/chaincode/assets/ehr_asset $PROJECT_ROOT/chaincode/ehr/vendor
 
 cp -r chaincode/accesslist minifabric/chaincode
 cp -r chaincode/ehr minifabric/chaincode
